f047a4a7ebbb0699ee378a73b5302de982ff2efe
Blockincraft <86124953+Bl0ckincraft@users.noreply.github.com>
2023-04-16 16:48:30 +0200

    Documentation du code

--- drapeau.py
+++ drapeau.py
@@ -3,11 +3,30 @@ import math
 from PIL import Image, ImageTk
 
 
+# Il s'agit d'une classe utilitaire pour pouvoir faire des calculs de position avec un triangle (notament savoir si un
+# point P(x; y) est à l'intérieur).
 class Triangle:
     def getArea(self):
+        """
+        Calcul de l'aire du triangle, on en a besoin pour savoir si un point P(x; y) est à l'intérieur.
+
+        :return: l'aire du triangle.
+        """
+
         return abs(0.5 * ((self.x1 * self.y2 + self.x2 * self.y3 + self.x3 * self.y1) - (self.y1 * self.x2 + self.y2 * self.x3 + self.y3 * self.x1)))
 
     def __init__(self, x1: int, y1: int, x2: int, y2: int, x3: int, y3: int):
+        """
+        Constructeur classique de la classe.
+
+        :param x1: Coordonnée x du premier point.
+        :param y1: Coordonnée y du premier point.
+        :param x2: Coordonnée x du deuxième point.
+        :param y2: Coordonnée y du deuxième point.
+        :param x3: Coordonnée x du troisième point.
+        :param y3: Coordonnée y du troisième point.
+        """
+
         self.x1 = x1
         self.y1 = y1
         self.x2 = x2
@@ -16,23 +35,58 @@ class Triangle:
         self.y3 = y3
 
     def isLocationIn(self, x: int, y: int):
+        """
+        On vérifie si le point P(x; y) se trouve dans le triangle.
+        Pour cela, on calcul la somme de l'aire de trois triangle formé à partir de deux points du triangle actuel et du
+        point P(x; y), ainsi si la somme de ces aires est égal à l'aire du triangle actuel alors le point se trouve bien
+        dans le triangle.
+
+        :param x: Coordonnée x du point.
+        :param y: Coordonnée y du point.
+        :return: Si le point se trouve dans le triangle.
+        """
         return Triangle(self.x1, self.y1, self.x2, self.y2, x, y).getArea() + Triangle(self.x1, self.y1, x, y, self.x3, self.y3).getArea() + Triangle(x, y, self.x2, self.y2, self.x3, self.y3).getArea() == self.getArea()
 
 
+# Il s'agit d'une classe utilitaire pour pouvoir faire des calculs de position avec une étoile (notament savoir si un
+# point P(x; y) est à l'intérieur).
 class Star:
-    def __init__(self, xCenter: int, yCenter: int, nearLength: int, farLength: int, startAngle: float):
+    def __init__(self, xCenter: int, yCenter: int, nearRadius: int, farRadius: int, rotation: float):
+        """
+        Constructeur classique de la classe.
+
+        :param xCenter: Coordonnée x du centre de l'étoile.
+        :param yCenter: Coordonnée y du centre de l'étoile.
+        :param nearRadius: Rayon du cercle le plus petit.
+        :param farRadius: Rayon du cercle le plus grand.
+        :param rotation: Rotation de l'étoile.
+        """
+
+        # L'étoile est constitué de 5 triangles que l'on génère par compréhension à l'aide du cercle trigonométrique et
+        # du module math. (Dans ce module, les angles pour le sinus et le cosinus doivent être en radians.)
         self.triangles = [
             Triangle(
-                int(math.cos((340 + i * 360 / 5 + startAngle) / 180 * math.pi) * nearLength) + xCenter,
-                int(math.sin((340 + i * 360 / 5 + startAngle) / 180 * math.pi) * nearLength) + yCenter,
-                int(math.cos((200 + i * 360 / 5 + startAngle) / 180 * math.pi) * nearLength) + xCenter,
-                int(math.sin((200 + i * 360 / 5 + startAngle) / 180 * math.pi) * nearLength) + yCenter,
-                int(math.cos((90 + i * 360 / 5 + startAngle) / 180 * math.pi) * farLength) + xCenter,
-                int(math.sin((90 + i * 360 / 5 + startAngle) / 180 * math.pi) * farLength) + yCenter,
+                # On cast les résultats en integers car sinon l'étoile n'est pas bien formée probablement à cause des
+                # arrondis.
+                int(math.cos((340 + i * 360 / 5 + rotation) / 180 * math.pi) * nearRadius) + xCenter,
+                int(math.sin((340 + i * 360 / 5 + rotation) / 180 * math.pi) * nearRadius) + yCenter,
+                int(math.cos((200 + i * 360 / 5 + rotation) / 180 * math.pi) * nearRadius) + xCenter,
+                int(math.sin((200 + i * 360 / 5 + rotation) / 180 * math.pi) * nearRadius) + yCenter,
+                int(math.cos((90 + i * 360 / 5 + rotation) / 180 * math.pi) * farRadius) + xCenter,
+                int(math.sin((90 + i * 360 / 5 + rotation) / 180 * math.pi) * farRadius) + yCenter,
             ) for i in range(5)
         ]
 
     def isLocationIn(self, x: int, y: int):
+        """
+        On vérifie si le point P(x; y) se trouve dans l'étoile.
+        Pour cela, on vérifie si le point est dans un des triangles qui composent l'étoile.
+
+        :param x: Coordonnée x du point.
+        :param y: Coordonnée y du point.
+        :return: Si le point se trouve dans l'étoile.
+        """
+
         for triangle in self.triangles:
             if triangle.isLocationIn(x, y):
                 return True
@@ -40,26 +94,33 @@ class Star:
         return False
 
 
+# Cette fonction renvoie le drapeau du pays demandé ou None si le drapeau n'est pas implémenté.
 def getFlag(name: str):
+    # On génère le nom de la fonction associé au drapeau avec le format suivant : "make[NomDuPays]Flag".
+    # Avec ce fonctionnement, on rend le code plus maintenable puisqu'il suffit de créer une nouvelle fonction pour
+    # implémenter un nouveau drapeau sans avoir à modifier celle-ci.
     functionName = f"make{name.title().replace(' ', '')}Flag"
 
+    # On vérifie que la fonction existe.
     if functionName in globals():
+        # On renvoie le résultat de la fonction transformé en image tkinter.
         return ImageTk.PhotoImage(image=eval(f"{functionName}()"), size=(600, 300))
 
 
 finlandFlag = None
 
 
-def initFlags(flags: list):
-    for flag in flags:
-        getFlag(flag)
-
-
+# Une fonction pour faire le drapeau de la Finlande avec PIL sans utiliser de formes prédéfinies.
 def makeFinlandFlag():
+    # Import de la variable globale finlandFlag.
     global finlandFlag
+    # S'il a déja été fait durant cette instance de l'application, on ne le refait pas et donc on n'entre pas dans le
+    # if.
     if finlandFlag is None:
+        # Création de l'image vide.
         flag = Image.new("RGB", (600, 300))
 
+        # Coloration de l'image.
         for x in range(600):
             for y in range(300):
                 if 165 < x <= 266 or 99 < y <= 200:
@@ -67,21 +128,30 @@ def makeFinlandFlag():
                 else:
                     flag.putpixel((x, y), (255, 255, 255))
 
+        # Sauvegarde de l'image pour ne pas la recréer durant la même instance de l'application.
         finlandFlag = flag
 
+    # On renvoie le drapeau.
     return finlandFlag
 
 
 czechRepublicFlag = None
 
 
+# Une fonction pour faire le drapeau de la Tchéquie avec PIL sans utiliser de formes prédéfinies.
 def makeCzechRepublicFlag():
+    # Import de la variable globale czechRepublicFlag.
     global czechRepublicFlag
+    # S'il a déja été fait durant cette instance de l'application, on ne le refait pas et donc on n'entre pas dans le
+    # if.
     if czechRepublicFlag is None:
+        # Création de l'image vide.
         flag = Image.new("RGB", (600, 300))
 
+        # Création de l'objet utilitaire pour réaliser le triangle bleu du drapeau.
         blueTriangle = Triangle(0, 0, 223, 150, 0, 299)
 
+        # Coloration de l'image.
         for x in range(600):
             for y in range(300):
                 if blueTriangle.isLocationIn(x, y):
@@ -91,21 +161,30 @@ def makeCzechRepublicFlag():
                 else:
                     flag.putpixel((x, y), (215, 20, 26))
 
+        # Sauvegarde de l'image pour ne pas la recréer durant la même instance de l'application.
         czechRepublicFlag = flag
 
+    # On renvoie le drapeau.
     return czechRepublicFlag
 
 
 togoFlag = None
 
 
+# Une fonction pour faire le drapeau du Togo avec PIL sans utiliser de formes prédéfinies.
 def makeTogoFlag():
+    # Import de la variable globale togoFlag.
     global togoFlag
+    # S'il a déja été fait durant cette instance de l'application, on ne le refait pas et donc on n'entre pas dans le
+    # if.
     if togoFlag is None:
+        # Création de l'image vide.
         flag = Image.new("RGB", (600, 300))
 
+        # Création de l'objet utilitaire pour réaliser l'étoile blanche du drapeau.
         whiteStar = Star(90, 90, 20, 60, 180)
 
+        # Coloration de l'image.
         for x in range(600):
             for y in range(300):
                 if whiteStar.isLocationIn(x, y):
@@ -117,6 +196,8 @@ def makeTogoFlag():
                 else:
                     flag.putpixel((x, y), (255, 206, 0))
 
+        # Sauvegarde de l'image pour ne pas la recréer durant la même instance de l'application.
         togoFlag = flag
 
+    # On renvoie le drapeau.
     return togoFlag
--- principal.py
+++ principal.py
@@ -2,6 +2,8 @@ import drapeau
 import informations
 import tkinter as tk
 
+# On entre ici la liste des pays à afficher, par l'utilisation de cette liste, le code est plus maintenable puisqu'il
+# suffit d'ajouter son nom ici et de créer une nouvelle fonction dans drapeau.py pour ajouter un nouveau pays.
 PAYS = [
     'Finland',
     'Czech Republic',
@@ -12,6 +14,18 @@ currentId = -1
 
 
 def createButtonClickHandler(id):
+    """
+    On utilise une autre fonction pour générer le click handler des boutons de sélection des pays car avec une fonction
+    lambda, on rencontre un problème.
+    Le problème est que puisque le lamba est utilisé dans une boucle for et qu'il utilise l'index de la boucle alors, il
+    ne créer pas de copie de l'index et utilise alors la même instance ce qui a pour effet d'utiliser la dernière valeur
+    de l'index. (Pour une boucle à trois répétitions d'index "i", lambda: show(i) correspondait à show(2) dans chaque
+    répétition car lors de la dernière execution de la boucle, i = 2.)
+
+    :param id: identifiant du pays.
+    :return: le click handler du bouton.
+    """
+
     def buttonClickHandler():
         show(id)
 
@@ -19,34 +33,51 @@ def createButtonClickHandler(id):
 
 
 def show(id: int):
+    """
+    Cette fonction va afficher les données d'un pays en fonction de son identifiant dans la liste PAYS ci-dessus.
+
+    :param id: identifiant du pays.
+    """
+
+    # Import de la variable globale currentId.
     global currentId
+    # Si les données du pays ne sont pas affichées alors on les affiche.
     if id != currentId:
+        # On change currentId pour savoir que ce sont les données de ce pays qui sont maintenant affichées.
         currentId = id
+        # On supprime les widgets déja présent dans le conteneur de données.
         for widget in dataContainer.winfo_children():
             widget.destroy()
 
+        # On récupère le drapeau du pays à l'aide du fichier drapeau.py.
         flag = drapeau.getFlag(PAYS[id])
 
+        # Si l'on a le drapeau alors on l'ajoute dans notre conteneur
         if flag is not None:
             label = tk.Label(dataContainer, image=flag)
             label.image = flag
             label.pack()
 
 
+# On créer notre fenêtre tkinter et on lui donne un nom.
 root = tk.Tk()
 root.title("Informations des pays")
 
+# On créer une barre de boutons avec un bouton pour chaque pays à l'intérieur.
 buttonBar = tk.Frame(root)
 for i in range(len(PAYS)):
     button = tk.Button(buttonBar, text=PAYS[i], command=createButtonClickHandler(i))
     button.grid(row=0, column=i, padx=5)
 
+# On créer un conteneur pour stocker les données à afficher dont le drapeau, la population...
 dataContainer = tk.Frame(root)
 emptyLabel = tk.Label(dataContainer, text="Vous pouvez voir les informations d'un pays en cliquant sur le bouton associé juste au dessus.", padx=10)
 emptyLabel.pack()
 
+# On ajoute nos conteneurs dans la fenêtre.
 buttonBar.grid(row=0, column=0, pady=20, padx=5)
 dataContainer.grid(row=1, column=0, pady=5, padx=5)
 
+# On lance la boucle principale de notre fenêtre tkinter.
 root.mainloop()
 
